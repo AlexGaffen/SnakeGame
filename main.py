4a99bb1b6b47dec24e3fe44fbc8006e97ea41aca
Alex Gaffen <alexgaffen@gmail.com>
2023-03-24 18:53:46 -0400

    Snake Length and Body

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import time
 import pygame
 from pygame.locals import *
 
+SIZE = 40
+
 class Snake:
-    def __init__(self, parent_screen):
+    def __init__(self, parent_screen, length):
+        self.length = length
+        self.direction = "DOWN"
         self.block = pygame.image.load("resources/block.jpg").convert()
-        self.x = 100
-        self.y = 100
+        self.x = [SIZE]*length
+        self.y = [SIZE]*length
         self.ps = parent_screen
 
     def draw(self):
         self.ps.fill((110, 110, 5))
-        self.ps.blit(self.block, (self.x, self.y))
+        for i in range(self.length):
+            self.ps.blit(self.block, (self.x[i], self.y[i]))
         pygame.display.flip()
 
     def moveu(self):
@@ -29,12 +35,30 @@ class Snake:
         self.x += 10
         self.draw()
 
+    def step(self):
+
+        for i in range(self.length-1,0,-1):
+            self.x[i] = self.x[i-1]
+            self.y[i] = self.y[i-1]
+
+        if self.direction == "UP":
+            self.y[0] -= SIZE
+        if self.direction == "DOWN":
+            self.y[0] += SIZE
+        if self.direction == "LEFT":
+            self.x[0] -= SIZE
+        if self.direction == "RIGHT":
+            self.x[0] += SIZE
+
+        self.draw()        
+
+
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((500,500))
+        self.surface = pygame.display.set_mode((1000,800))
         self.surface.fill((110, 110, 5))  
-        self.snake = Snake(self.surface)
+        self.snake = Snake(self.surface, 6)
         self.snake.draw()
 
     def run(self): 
@@ -48,16 +72,19 @@ class Game:
                         running = False
                     
                     if event.key == K_UP:
-                        self.snake.moveu()
+                        self.snake.direction = "UP"
                     if event.key == K_DOWN:
-                        self.snake.moved()
+                        self.snake.direction = "DOWN"
                     if event.key == K_LEFT:
-                        self.snake.movel()
+                        self.snake.direction = "LEFT"
                     if event.key == K_RIGHT:
-                        self.snake.mover()
+                        self.snake.direction = "RIGHT"
 
                 elif event.type == QUIT:
                     running = False   
+        
+            self.snake.step()
+            time.sleep(0.2)
 
 if __name__ == "__main__":
     game = Game()
